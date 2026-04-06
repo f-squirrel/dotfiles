33c7fdf7c8e3a89bd030aa05322571da85a869c3
f-squirrel <dmitry.b.danilov@gmail.com>
2026-04-06 15:55:01 +0300

    refactor(profiles): make minimal self-contained, default imports full

--- home/default.nix
+++ home/default.nix
@@ -1,44 +1,5 @@
 {
-  username,
-  pkgs,
-  lib,
-  ...
-}:
-{
   imports = [
-    ./modules/atuin.nix
-    ./modules/btop.nix
-    ./modules/eza.nix
-    ./modules/fzf.nix
-    ./modules/git.nix
-    ./modules/neovim.nix
-    ./modules/packages-cli.nix
-    ./modules/packages-darwin.nix
-    ./modules/ripgrep.nix
-    ./modules/starship.nix
-    ./modules/zellij.nix
-    ./modules/zoxide.nix
-    ./modules/zsh.nix
+    ./profiles/full.nix
   ];
-
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
-    builtins.elem (lib.getName pkg) [
-      "claude-code"
-    ];
-
-  home = {
-    inherit username;
-    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
-
-    # Do not change this when upgrading Home Manager.
-    # See https://home-manager.nix.community/options.html#opt-home.stateVersion
-    stateVersion = "24.11";
-  };
-
-  fonts.fontconfig.enable = true;
-
-  programs.home-manager.enable = true;
-
-  targets.genericLinux.enable = pkgs.stdenv.isLinux;
 }
--- home/profiles/minimal.nix
+++ home/profiles/minimal.nix
@@ -1,5 +1,44 @@
 {
+  username,
+  pkgs,
+  lib,
+  ...
+}:
+{
   imports = [
-    ../default.nix
+    ../modules/atuin.nix
+    ../modules/btop.nix
+    ../modules/eza.nix
+    ../modules/fzf.nix
+    ../modules/git.nix
+    ../modules/neovim.nix
+    ../modules/packages-cli.nix
+    ../modules/packages-darwin.nix
+    ../modules/ripgrep.nix
+    ../modules/starship.nix
+    ../modules/zellij.nix
+    ../modules/zoxide.nix
+    ../modules/zsh.nix
   ];
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "claude-code"
+    ];
+
+  home = {
+    inherit username;
+    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
+
+    # Do not change this when upgrading Home Manager.
+    # See https://home-manager.nix.community/options.html#opt-home.stateVersion
+    stateVersion = "24.11";
+  };
+
+  fonts.fontconfig.enable = true;
+
+  programs.home-manager.enable = true;
+
+  targets.genericLinux.enable = pkgs.stdenv.isLinux;
 }
