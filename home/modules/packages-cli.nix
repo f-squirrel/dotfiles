06f16b2ad729b399cd30ab6130fbbdfd0c63681b
f-squirrel <dmitry.b.danilov@gmail.com>
2026-04-07 21:42:25 +0300

    fix(packages-cli): streamline package list formatting

--- home/modules/packages-cli.nix
+++ home/modules/packages-cli.nix
@@ -1,17 +1,15 @@
 { pkgs, lib, ... }:
 {
-  home.packages =
-    with pkgs;
-    [
-      curl
-      duf
-      dust
-      fd
-      glow
-      gnumake
-      jq
-      just
-      shellcheck
-      wget
-    ]
+  home.packages = with pkgs; [
+    curl
+    duf
+    dust
+    fd
+    glow
+    gnumake
+    jq
+    just
+    shellcheck
+    wget
+  ];
 }
