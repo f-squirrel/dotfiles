572e2dc694d40248684ec4196dca68c975183279
f-squirrel <dmitry.b.danilov@gmail.com>
2026-04-07 17:02:29 +0300

    refactor(neovim): use xdg.configFile to symlink config dir

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -4,10 +4,9 @@
     enable = true;
     vimAlias = true;
     viAlias = true;
-    # Use extraLuaConfig so home-manager generates a single init.lua,
-    # avoiding the init.vim vs init.lua conflict it would otherwise cause.
-    initLua = builtins.readFile ../config/nvim/init.lua;
   };
+
+  xdg.configFile."nvim".source = ../config/nvim;
 
   # tree-sitter CLI is required by nvim-treesitter (main branch) to compile parsers
   home.packages = [ pkgs.tree-sitter ];
