d5a68fe140cf8339ff5cf19c6d5ebc23e89f1184
f-squirrel <dmitry.b.danilov@gmail.com>
2026-04-07 17:47:16 +0300

    fix(nix): format

--- home/modules/packages-cli.nix
+++ home/modules/packages-cli.nix
@@ -1,21 +1,24 @@
 { pkgs, lib, ... }:
 {
-  home.packages = with pkgs; [
-    curl
-    devcontainer
-    duf
-    dust
-    fd
-    glow
-    gnumake
-    jq
-    just
-    openconnect
-    shellcheck
-    tokei
-    vale
-    wget
-  ] ++ lib.optionals pkgs.stdenv.isLinux [
-    xsel
-  ];
+  home.packages =
+    with pkgs;
+    [
+      curl
+      devcontainer
+      duf
+      dust
+      fd
+      glow
+      gnumake
+      jq
+      just
+      openconnect
+      shellcheck
+      tokei
+      vale
+      wget
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      xsel
+    ];
 }
