5c27930d3da89e0a371fb0dcc9211196e5cc8b30
f-squirrel <dmitry.b.danilov@gmail.com>
2026-04-07 17:03:09 +0300

    refactor(zellij): use xdg.configFile to symlink config dir

--- home/modules/zellij.nix
+++ home/modules/zellij.nix
@@ -2,10 +2,7 @@
 {
   programs.zellij.enable = true;
 
-  xdg.configFile = {
-    "zellij/config.kdl".source = ../config/zellij/config.kdl;
-    "zellij/layouts/default.kdl".source = ../config/zellij/layouts/default.kdl;
-  };
+  xdg.configFile."zellij".source = ../config/zellij;
 
   home.packages = [
     (pkgs.writeShellScriptBin "vscode-open" (builtins.readFile ../../scripts/utils/vscode-open.sh))
