546a342cbc23e5a9fb7aea269134f6b4e2a1f16f
f-squirrel <dmitry.b.danilov@gmail.com>
2026-04-08 22:53:11 +0300

    feat(starship): show command duration and finish time in prompt

--- home/modules/starship.nix
+++ home/modules/starship.nix
@@ -3,6 +3,17 @@ _: {
     enable = true;
     settings = {
       add_newline = false;
+
+      cmd_duration = {
+        min_time = 0;
+        format = "took [$duration]($style) ";
+      };
+
+      time = {
+        disabled = false;
+        format = "at [$time]($style) ";
+        time_format = "%T";
+      };
     };
   };
 }
